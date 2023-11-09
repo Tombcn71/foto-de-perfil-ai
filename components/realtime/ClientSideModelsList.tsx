a52cad22b11850d6dfc7c55418b75a66fff9cc23
Tom <tbvanreijn@gmail.com>
2023-11-09 14:31:31 +0100

    Translation

--- components/realtime/ClientSideModelsList.tsx
+++ components/realtime/ClientSideModelsList.tsx
@@ -60,7 +60,7 @@ export default function ClientSideModelsList({
       {models && models.length > 0 && (
         <div className="flex flex-col gap-4">
           <div className="flex flex-row gap-4 w-full justify-between items-center text-center">
-            <h1>Your models</h1>
+            <h1>Tus modelos</h1>
             <Link href="/overview/models/train" className="w-fit">
               <Button size={"sm"}>Train model</Button>
             </Link>
